e967495b310e5dd38d78f41fe1584ddd420d0169
sree821253-art <sree821253@gmail.com>
2026-01-11 09:14:55 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,19 +1,16 @@
 import java.util.Properties
 import java.io.FileInputStream
-
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
-
 val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("key.properties")
 if (keystorePropertiesFile.exists()) {
     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
-
 android {
     namespace = "com.example.darvin_app"
     compileSdk = 36
@@ -40,7 +37,7 @@ android {
         create("release") {
             keyAlias = System.getenv("BITRISEIO_ANDROID_KEYSTORE_ALIAS") ?: keystoreProperties["keyAlias"] as String?
             keyPassword = System.getenv("BITRISEIO_ANDROID_KEYSTORE_PRIVATE_KEY_PASSWORD") ?: keystoreProperties["keyPassword"] as String?
-            storeFile = System.getenv("BITRISEIO_ANDROID_KEYSTORE_PATH")?.let { file(it) } ?: keystoreProperties["storeFile"]?.let { file(it) }
+            storeFile = System.getenv("BITRISEIO_ANDROID_KEYSTORE_PATH")?.let { file(it) }
             storePassword = System.getenv("BITRISEIO_ANDROID_KEYSTORE_PASSWORD") ?: keystoreProperties["storePassword"] as String?
         }
     }
@@ -51,7 +48,6 @@ android {
         }
     }
 }
-
 flutter {
     source = "../.."
 }
